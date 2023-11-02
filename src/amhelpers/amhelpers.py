623a5dc4a205c719d6e68719916eb4103738b977
antmats <anton.mattsson94@gmail.com>
2023-11-02 14:06:13 +0100

    feat: add functions seed_hash and create_results_dir_from_config

--- src/amhelpers/amhelpers.py
+++ src/amhelpers/amhelpers.py
@@ -1,11 +1,16 @@
 import pydoc
-import yaml
+import copy
+import datetime
 import pickle
+import hashlib
 from os.path import join
 from pathlib import Path
 
+import yaml
+
+
 def create_object_from_dict(d, **default_kwargs):
-    '''Create an object from a dictionary.
+    """Create an object from a dictionary.
 
     Inspired by `iglovikov_helper_functions.config_parsing.utils`. [1]_
 
@@ -25,7 +30,7 @@ def create_object_from_dict(d, **default_kwargs):
     References
     ----------
     .. [1] https://github.com/ternaus/iglovikov_helper_functions/
-    '''
+    """
     if not isinstance(d, dict):
         raise TypeError("'d' should be of type 'dict'.")
     if not 'type' in d:
@@ -36,8 +41,9 @@ def create_object_from_dict(d, **default_kwargs):
         kwargs.setdefault(name, value)
     return pydoc.locate(class_type)(**kwargs)
 
+
 def get_class_from_str(s):
-    '''Get the uninitialized class `s`.
+    """Get the uninitialized class `s`.
 
     Parameters
     ----------
@@ -48,11 +54,12 @@ def get_class_from_str(s):
     -------
     class
         The uninitialized class `s`.
-    '''
+    """
     return pydoc.locate(s)
 
+
 def yield_nested_dict_values(d):
-    '''Yield (non-dict) values from a nested dict.
+    """Yield (non-dict) values from a nested dict.
 
     Code from [2]_.
 
@@ -69,24 +76,64 @@ def yield_nested_dict_values(d):
     References
     ----------
     .. [2] https://tutorial.eyehunts.com/python/python-get-all-values-from-nested-dictionary-example-code/
-    '''
+    """
     for v in d.values():
         if isinstance(v, dict):
             yield from yield_nested_dict_values(v)
         else:
             yield v
 
+
 def save_pickle(data, path, filename):
     Path(path).mkdir(parents=True, exist_ok=True)
     with open(join(path, filename + '.pickle'), 'wb') as f:
         pickle.dump(data, f)
+
 
 def save_yaml(data, path, filename, **kwargs):
     Path(path).mkdir(parents=True, exist_ok=True)
     with open(join(path, filename + '.yaml'), 'w') as f:
         yaml.dump(data, f, default_flow_style=False, **kwargs)
 
+
 def load_yaml(path):
     with open(path) as file:
         yaml_file = yaml.safe_load(file)
     return yaml_file
+
+
+def seed_hash(*args):
+    """Derive an integer hash from `args` to use as a random seed."""
+    args_str = str(args)
+    return int(hashlib.md5(args_str.encode('utf-8')).hexdigest(), 16) % (2**31)
+
+
+def create_results_dir_from_config(
+    config,
+    suffix=None,
+    update_config=False
+):
+    """Create a directory for storing results.
+
+    Parameters
+    ----------
+    config : dict
+        Configuration dictionary. Must have a key 'results' with a key 'path' 
+        specifying the path to the results directory.
+    suffix : str, optional
+        Suffix to append to the results directory name.
+    update_config : bool, optional
+        Whether to update the configuration dictionary with the new results path.
+    """
+    time_stamp = datetime.datetime.now().strftime('%Y%m%d_%H%M')
+    if suffix is not None:
+        time_stamp += '_' + suffix
+    results_path = join(config['results']['path'], time_stamp)
+    Path(results_path).mkdir(parents=True, exist_ok=True)
+    
+    if update_config:
+        config = copy.deepcopy(config)
+        config['results']['path'] = results_path
+        return results_path, config
+    else:
+        return results_path
